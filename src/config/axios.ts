c739f13377e13e2cb20d7a84d82da41cf78f1606
livingdolls <nanangyorozuya@gmail.com>
2023-01-04 00:39:07 +0700

    fix url

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const client = axios.create({
-	baseURL: "http://localhost:3001/api/v1/",
+	baseURL: "https://budget-app-production-8a62.up.railway.app/api/v1/",
 });
 
 export const clientPrivate = axios.create({
-	baseURL: "http://localhost:3001/api/v1/",
+	baseURL: "https://budget-app-production-8a62.up.railway.app/api/v1/",
 	headers: { "Content-Type": "application/json" },
 	withCredentials: true,
 });
